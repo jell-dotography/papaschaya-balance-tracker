0e21dd7a215742a6856441a6b6a72967f00ab9d1
Otoshi-gami <jell-dotography@dotography.com>
2016-07-07 19:17:42 +0700

    Failed attempt at making Income and Expense as subclasses of Balance.

--- src/main/scala/com/dotography/balancetracker/model/Balance.scala
+++ src/main/scala/com/dotography/balancetracker/model/Balance.scala
@@ -14,13 +14,15 @@ import net.liftweb.mapper.MappedLongForeignKey
 trait Balance[A <: Balance[A]] extends LongKeyedMapper[A] with IdPK {  
   self: A =>
   implicit val formats = net.liftweb.json.DefaultFormats
-//  class blah extends MappedLongForeignKey(this, IncomeCategory)
-//  def mappedCategory[O <: MappedLongForeignKey[O]]: MappedLongForeignKey[A, O] = new blah// with categoryField[A]
-  
-//  trait categoryField[A <: Balance[A]] extends MappedLong[A] {
-//    override def dbColumnName = "category_id"
-//  }
+  //def mappedCategory[O <: LongKeyedMapper[O]]: MappedLongForeignKey[A, O]// with categoryField[A]
+  abstract class mappedCategory[A <: Balance[A], O <: LongKeyedMapper[O]](a : A, b : LongKeyedMetaMapper[O]) extends MappedLongForeignKey[A, O](a, b) {
+    type B <: Balance[A]
+    type P <: LongKeyedMapper[O]
+    def getMe() : mappedCategory[B,P]
+    override def dbColumnName = "category_id"
+  }
 
+  def categoryM [A <: Balance[A], O <: LongKeyedMapper[O]] : mappedCategory[A,O]
   object amount extends MappedDecimal(this, MathContext.DECIMAL64, 2)
   object effectiveDate extends MappedDate(this) {
     override def dbColumnName = "effective_date"
@@ -32,18 +34,25 @@ trait Balance[A <: Balance[A]] extends LongKeyedMapper[A] with IdPK {
   def toJson = {
     import net.liftweb.json._
     import net.liftweb.json.JsonDSL._
-    JObject(List(id.asJsonField.orNull, mappedCategory.asJsonField.orNull))
+    //JObject(List(id.asJsonField.orNull, categoryM.asJsonField.orNull))
   }
 }
 
-//object Income extends Income with LongKeyedMetaMapper[Income]
-//
-//class Income extends Balance[Income] {
-//  override def getSingleton = Income
-//  object category extends blah// with categoryField[Income]
-//  override def mappedCategory = new blah
-//}
+object Income extends Income with LongKeyedMetaMapper[Income]
 
+class Income extends Balance[Income] {
+  override def getSingleton = Income
+  //object category extends MappedLongForeignKey(this, IncomeCategory)// with categoryField[Income]
+  //override def categoryM [Income] : mappedCategory[Income] = new mappedCategory[IncomeCategory](IncomeCategory)//= new category[IncomeCategory](IncomeCategory)
+  //override def mappedCategory = category.asInstanceOf[MappedLongForeignKey[Income, IncomeCategory]]
+  class category(income : Income, incomeCategory : IncomeCategory) extends mappedCategory[Income, IncomeCategory](income, incomeCategory.asInstanceOf[LongKeyedMetaMapper[IncomeCategory]]) {
+    type B = Income
+    type P = IncomeCategory
+    def getMe() : mappedCategory[Income, IncomeCategory] = this
+  }
+  object me extends category(this, IncomeCategory)
+  override def categoryM [A <: Balance[A], O <: LongKeyedMapper[O]]: mappedCategory[Income,IncomeCategory]= me.getMe
+}
 
 //object Expense extends Expense with LongKeyedMetaMapper[Expense]
 //
